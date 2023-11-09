b860e99c64b82fefb2d2a14b25285d4b653adb5d
KevinOBautista <kevinbautista2103@gmail.com>
2023-11-09 16:53:28 -0500

    first commit

---
+++ src/components/Home/index.js
@@ -0,0 +1,62 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import LogoTitle from '../../assets/images/logo-s.png'
+// import Logo from './Logo'
+import './index.scss'
+import Logo from './Logo'
+
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  const nameArray = 'Kevin Bautista'.split('')
+  const jobArray = 'A Web Developer.'.split('')
+
+  useEffect(() => {
+    setTimeout(() => {
+      return setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
+
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            {/* <img
+              src={LogoTitle}
+              alt="JavaScript Developer Name, Web Developer Name"
+            /> */}
+            &nbsp;
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={29}
+            />
+          </h1>
+          <h2>Front End Developer / Back End Developer</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <Logo />
+      </div>
+
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Home
